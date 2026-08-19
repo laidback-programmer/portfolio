aec4347a58861d019f1baf8c2eeb8ee6d7d4b8ca
laidback-programmer <atwork.arya@gmail.com>
2026-08-19 13:10:09 +0530

    uncommented activites and contact form

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 import { About } from "@/components/about";
 import { GreetingLoader } from "@/components/greeting-loader";
 import { Hero } from "@/components/hero";
-// import { Projects } from "@/components/projects";
-// import { Activities } from "@/components/activities"
+import { Projects } from "@/components/projects";
+import { Activities } from "@/components/activities"
 // import { Skills } from "@/components/skills";
-// import { ContactForm } from "@/components/contact_form";
+import { ContactForm } from "@/components/contact_form";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -24,13 +24,15 @@ export default function Home() {
         <About />
       </section>
 
-      {/* <section id="projects">
+      <section id="projects">
         <Projects />
       </section>
 
-      <section id="skills">
-        <Skills />
-      </section>
+      {/*
+        <section id="skills">
+          <Skills />
+        </section>
+      */}
 
       <section id="activities">
         <Activities />
@@ -38,7 +40,7 @@ export default function Home() {
 
       <section id="contact">
         <ContactForm />
-      </section> */}
+      </section>
     </main>
   );
 }
